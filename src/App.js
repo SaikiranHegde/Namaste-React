99fd049f8da1b56b17850842495039825f23722d
SaikiranHegde <saikiranhegde0793@gmail.com>
2024-03-19 16:10:55 +0530

    Props, Functional Component and Iteration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import appLogo from './assests/app-logo.svg';
+import { defaultTo, map } from "ramda";
 
 const Header = () => {
   return (
@@ -22,43 +23,59 @@ const Header = () => {
 }
 
 // Inline CSS
-const RestuarantCard = () => {
+
+const RestaurantCard = (props) => {
   return (
     <section className="rest-card" style={{
       width: '250px',
       height: '250px',
     }}>
-      <img alt="Asha Tiffins" className="rest-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iyhixkvgcssweqmvesfk" />
-      <div className="rest-name">Asha Tiffins</div>
-      <div className="rest-info">South Indian, North Indian, Chinese</div>
+      <img alt={props.restData?.name} className="rest-img" src={props.restData?.image} />
+      <div className="rest-name">{defaultTo('Asha Tiffins', props.restData.name)}</div>
+      <div className="rest-info">{props.restData?.cuisines}</div>
       <div className="rest-info">4.5 Stars</div>
       <div className="rest-info">15 - 20 mins</div>
     </section>
   );
-}
+} 
+
+// Mock Data
+const RestaurantData = [
+  {
+    id: 1,
+    name: "Asha Tiffins",
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iyhixkvgcssweqmvesfk",
+    cuisines: "South Indian, North Indian, Chinese",
+  },
+  {
+    id: 2,
+    name: "Udupi Food Hub",
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hpulkfkzoh6kamqaaxdx",
+    cuisines: "South Indian, North Indian, Chats",
+  },
+];
 
 const Body = () => {
   return (
     <section className="content-container">
       <div className="search-bar">Search</div>
       <div className="rest-container">
-        <RestuarantCard />
-        <RestuarantCard />
-        <RestuarantCard />
-        <RestuarantCard />
-        <RestuarantCard />
-        <RestuarantCard />
-        <RestuarantCard />
+        {map(
+          (restData) => (
+            <RestaurantCard restData={restData} key={restData.id} />
+          ),
+          RestaurantData
+        )}
       </div>
     </section>
   );
-}
+};
 
 const Footer = () => {
   return (
-    <section>
-      Footer
-    </section>
+    <section></section>
   );
 }
 
