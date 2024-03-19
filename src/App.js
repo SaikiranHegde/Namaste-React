8682c99e8792c76552a8428c2c7bcc6535e90b6a
SaikiranHegde <saikiranhegde0793@gmail.com>
2024-03-19 11:43:03 +0530

    Basics 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,76 @@
-import './App.css';
+import "./App.css";
+import appLogo from './assests/app-logo.svg';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HeadingComponent />
-      </header>
-    </div>
+    <section className="header-container">
+      <div className="logo-container">
+        <img
+          src={appLogo}
+          alt="logo"
+          className="app-logo"
+        />
+      </div>
+      <div className="nav-items">
+        <div>Home</div>
+        <div>About Us</div>
+        <div>Contact Us</div>
+        <div>Cart</div>
+      </div>
+    </section>
   );
 }
 
-// React Element
-const heading2 = <h2 className="heading">Hello, React 2!</h2>;
-
-// Variable
-const count = 1000;
-
-// React Component
-const HeadingComponent = () => {
+// Inline CSS
+const RestuarantCard = () => {
   return (
-    <div>
-      <h1 className="heading">Hello, React!</h1>
-      {heading2}
-      {count + 100}
-    </div>
+    <section className="rest-card" style={{
+      width: '250px',
+      height: '250px',
+    }}>
+      <img alt="Asha Tiffins" className="rest-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iyhixkvgcssweqmvesfk" />
+      <div className="rest-name">Asha Tiffins</div>
+      <div className="rest-info">South Indian, North Indian, Chinese</div>
+      <div className="rest-info">4.5 Stars</div>
+      <div className="rest-info">15 - 20 mins</div>
+    </section>
   );
-};
+}
+
+const Body = () => {
+  return (
+    <section className="content-container">
+      <div className="search-bar">Search</div>
+      <div className="rest-container">
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+      </div>
+    </section>
+  );
+}
+
+const Footer = () => {
+  return (
+    <section>
+      Footer
+    </section>
+  );
+}
+
+
+const  App = () => {
+  return (
+    <section className="app-container">
+      <Header />
+      <Body />
+      <Footer />
+    </section>
+  );
+}
 
 export default App;
